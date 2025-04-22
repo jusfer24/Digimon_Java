db3d34b795f974b84776faac13a2f5b7e9b33e98
KamiNSoria <cami.cnns@gmail.com>
2025-04-21 23:51:56 -0500

    Update Main.java
    aaaa

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,20 @@ public class Main {
         Digimon d3 = new Digimon("Gabumon", "Hielo", 130, "Flecha Glacial", "Activo");
         Digimon d4 = new Digimon("Tentomon", "Eléctrico", 110, "Rayo Supersónico", "Inactivo");
         Digimon d5 = new Digimon("Gatomon", "Luz", 120, "Garra Celestial", "Activo");
+
         ArrayList<Digimon> digimones = new ArrayList<>();
         digimones.add(d1);
         digimones.add(d2);
         digimones.add(d3);
         digimones.add(d4);
         digimones.add(d5);
+
         ColaDigimon cola = new ColaDigimon();
 
-        cola.R1(digimones);
-
-        System.out.println(digimones);
+        cola.R1(digimones);  // Ahora funciona
         cola.mostrar();
-//        cola.R3(digimones);
         cola.R4(digimones);
-        cola.R1(digimones);
+        cola.R1(digimones);  // Puedes volver a encolar si quieres
     }
 }
+
